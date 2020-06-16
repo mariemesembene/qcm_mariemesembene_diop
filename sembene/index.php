e756084ffbf63c944152c109b61d6ed153e087a7
mariemesembene <62621045+mariemesembene@users.noreply.github.com>
2020-06-16 09:46:30 +0000

    Add files via upload

---
+++ sembene/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div id="cont">
+</div>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+    <script>
+    $( "#cont" ).load( "pagedeconnexion.php" );
+    </script>
+</body>
+</html>
